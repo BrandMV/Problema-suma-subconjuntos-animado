f2b74b21849c14dd30575570412de0e7745ca3d8
BrandMV <brandonmv2001@gmail.com>
2021-11-25 00:03:52 -0600

    code tabs

--- js/main.js
+++ js/main.js
@@ -29,3 +29,46 @@ navItem4.addEventListener('click', e => {
     navItem3.classList.remove('active')
     navItem4.classList.add('ia-active')
 })
+
+//*Para pestañas de códigos
+const openCode = (leng) => {
+    let btn = leng+"-btn"
+    console.log(btn);
+    let i
+    let code = document.getElementsByClassName("codigo")
+    let codebtn = document.getElementsByClassName("codigo-btn")
+    for(i=0;i<code.length;i++){
+        code[i].style.display = "none"
+        
+    }
+    for(i=0;i<codebtn.length;i++){
+        codebtn[i].setAttribute('style', "background: white;")
+        
+    }
+    document.getElementById(btn).setAttribute('style', "border-bottom: none;background: var(--morningGlory);")
+
+
+    document.getElementById(leng).style.display = "block"
+}
+
+const openCodeDP = (leng) => {
+    let btn = leng+"-btn"
+    console.log(btn);
+    let i
+    let code = document.getElementsByClassName("codigo-dp")
+    let codebtn = document.getElementsByClassName("codigo-btn-dp")
+    for(i=0;i<code.length;i++){
+        code[i].style.display = "none"
+        
+    }
+    for(i=0;i<codebtn.length;i++){
+        codebtn[i].setAttribute('style', "background: white;")
+        
+    }
+    document.getElementById(btn).setAttribute('style', "border-bottom: none;background: var(--morningGlory);")
+
+
+    document.getElementById(leng).style.display = "block"
+}
+
+
