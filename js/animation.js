52f637936f8e47bc1a969c34e608f97e38a28c5d
Alfredo Martinez <alfredomartinezruiz01@gmail.com>
2021-12-05 15:43:38 -0600

    Divs arreglados en modal

--- js/animation.js
+++ js/animation.js
@@ -1,29 +1,29 @@
 const txtNum = document.getElementById("output");
 const bRange = document.getElementById("bRange");
 
-txtNum.textContent = 1 + "s";
-
 bRange.addEventListener("input", function() {
     txtNum.textContent = bRange.value + "s";
 });
 
-const agregarI = document.getElementById("agregarI");
+const arreglo = [];
+const agregarI = document.getElementById("agregar-arreglo");
+const contenedor = document.querySelector('#contenedor-arreglo');
+
 const inpTxt = document.querySelector(".input-num");
 const tamArr = document.getElementById("numArr");
 
-var i = 1;
+agregarI.addEventListener("click", function() {
 
-$("#agregarI").addEventListener("click", function() {
+    tamArr.textContent = "n: " + arreglo.length;
+    var nuevo_valor = document.createElement('div');
+    nuevo_valor.className = "bd-highlight d-flex justify-content-start m-1 w-100 align-items-center";
 
-    i++;
-    tamArr.textContent = "n" + i;
-    var newDiv = document.createElement('div');
-    newDiv.innerHTML =
-        '<div class="bd-highlight d-flex justify-content-center m-1">\
-        <input type="number" class="mx-1 input-num text-center" value="1" min="1" name="array[]">\
-        <div class="bg-danger b-redondeado px-2 fs-2 minus ">\
-            <p class="text-white fw-bolder minus">-</p>\
-        </div></div>';
+    
 
-    $("#cont-arreglo").appendChild(newDiv);
+    nuevo_valor.innerHTML = `<input type="number" class="input-num text-center w-75" value="1" min="1" name="array[]">
+    <div class="bg-danger b-redondeado px-2 fs-2 minus">
+        <p class="text-white fw-bolder minus">-</p>
+    </div>`;
+
+    contenedor.insertBefore(nuevo_valor, agregarI);
 });
