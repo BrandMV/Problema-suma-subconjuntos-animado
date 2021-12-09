8f17b143ae4dd07787340d1fe888b2a5e034dc3e
Alfredo Martinez <alfredomartinezruiz01@gmail.com>
2021-12-08 20:59:20 -0600

    Función validar datos comentada

--- js/animation.js
+++ js/animation.js
@@ -145,10 +145,7 @@ btn_guardar.addEventListener("click", function() {
  */
 btn_simular.addEventListener("click", function() {
     // Validamos que los datos estén correctos
-    if (!validarDatos()) {
-        return;
-    }
-
+   
     // Indexamos el algoritmo seleccionado
     if (algoritmos[configs.algoritmo] == 'Fuerza bruta') {
 
