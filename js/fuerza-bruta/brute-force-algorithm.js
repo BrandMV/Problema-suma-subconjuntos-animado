350be4029785ce421400c5d900411c19a5c931ae
Alfredo Martinez <alfredomartinezruiz01@gmail.com>
2021-12-09 19:30:59 -0600

    Sincronía casi lista

--- js/fuerza-bruta/brute-force-algorithm.js
+++ js/fuerza-bruta/brute-force-algorithm.js
@@ -13,66 +13,69 @@ let id = 1;
 
 export function main(obj) {
 
+    // Creamos el nodo raiz y lo mostramos
     root = new Arbol("", 0, "(" + obj.valores.length + "," + obj.suma + ")", 0);
     ui.mostrarNodo(root);
+    
+    // Obtenemos el tiempo de retardo
     retardo = obj.velocidad * 1000;
+
+    // Mandamos a llamar al algoritmo
     esSumaConjunto(obj.valores, obj.valores.length, obj.suma, root);
-    /*
-    let nodo = agregarNodo(root, 10, 1, "(1, 1)", "der");
-    nodo = agregarNodo(nodo, 5, 2, "(2, 2)", "der");
-
-    nodo = agregarNodo(nodo, 8, 3, "(2, 2)", "der");
-    nodo = agregarNodo(nodo, 2, 4, "(2, 2)", "der");
-    ui.establecerResultado(nodo.id, false);
-    nodo = nodo.getPadre();
-
-    nodo = agregarNodo(nodo, 2, 5, "(2, 2)", "izq");
-    ui.establecerResultado(nodo.id, true);
-    ui.establecerResultado(nodo.getPadre().id, true);
-
-    nodo = nodo.getPadre().getPadre();
-    nodo = agregarNodo(nodo, 3, 6, "(2, 2)", "izq");*/
 }
 
+// Función síncrona
 async function esSumaConjunto(set, n, sum, nodo) {
     let resultado;
+    await sleep(retardo); // Retardamos antes de ejecutar el algoritmo
 
-    await sleep(retardo);
+    // Si se completó la suma del algoritmo
     if(sum == 0){
         ui.establecerResultado(nodo.id, true);
-        await sleep(retardo);
+        
+        await sleep(retardo); // Retardamos antes de volver al padre
         return true;
     }
+
+    // Si ya no hay más elementos en el conjunto
     if(sum < 0 || (n == 0 && sum != 0)){
         ui.establecerResultado(nodo.id, false);
-        await sleep(retardo);
+
+        await sleep(retardo); // Retardamos antes de volver al padre
         return false;
     }
 
+    // Si el elemento actual es mayor a la suma
     if (set[n - 1] > sum) {
         id++;
-        const nodo_der = agregarNodo(nodo, set[n - 1], id, "(" + (n - 1) + "," + sum + ")", "der");
-        resultado = await esSumaConjunto(set, n - 1, sum, nodo_der);
+        const nodo_der = agregarNodo(nodo, set[n - 1], id, "(" + (n - 1) + "," + sum + ")", "der"); // Agregamos el nodo
+        resultado = await esSumaConjunto(set, n - 1, sum, nodo_der); // Esperamos la llamada recursiva
         ui.establecerResultado(nodo.id, resultado);
-        await sleep(retardo);
+
+        await sleep(retardo); // Retardamos antes de volver al padre
         return resultado;
     }
 
+    // Considerando no tomar el elemento actual
     id++;
-    const nodo_der = agregarNodo(nodo, set[n - 1], id, "(" + (n - 1) + "," + sum + ")", "der");
-    resultado = await esSumaConjunto(set, n - 1, sum, nodo_der);
+    const nodo_der = agregarNodo(nodo, set[n - 1], id, "(" + (n - 1) + "," + sum + ")", "der"); // Agregamos el nodo
+    resultado = await esSumaConjunto(set, n - 1, sum, nodo_der); // Esperamos la llamada recursiva
     
     if(resultado){
         ui.establecerResultado(nodo.id, resultado);
-        await sleep(retardo);
+
+        await sleep(retardo); // Retardamos antes de volver al padre
         return resultado;
-    } else{
+    } 
+    
+    // Si no tomamos el elemento actual y da falso, mandamos a tomar el elemento actual
+    else{
         id++;
-        const nodo_izq = agregarNodo(nodo, set[n - 1], id, "(" + (n - 1) + "," + (sum - set[n -1]) + ")", "izq");
-        resultado = await esSumaConjunto(set, n - 1, sum - set[n - 1], nodo_izq)
-        
+        const nodo_izq = agregarNodo(nodo, set[n - 1], id, "(" + (n - 1) + "," + (sum - set[n -1]) + ")", "izq"); // Agregamos el nodo
+        resultado = await esSumaConjunto(set, n - 1, sum - set[n - 1], nodo_izq) // Esperamos la llamada recursiva
         ui.establecerResultado(nodo.id, resultado);
-        await sleep(retardo);
+
+        await sleep(retardo); // Retardamos antes de volver al padre
         return resultado;
     }
 }
