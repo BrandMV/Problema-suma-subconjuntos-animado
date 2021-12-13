518bc34b8c48fd6cfd14db9178b4b2f81f727e13
BrandMV <brandonmv2001@gmail.com>
2021-12-13 12:25:09 -0600

    popovers

--- js/main.js
+++ js/main.js
@@ -200,3 +200,9 @@ function verMas2(id){
     }
 
 }
+
+//*Popovers
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
